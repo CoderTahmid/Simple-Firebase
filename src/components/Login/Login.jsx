19217e5741451e7c5d65ec717d43e8f45f008724
CoderTahmid <tahmid.ibne@gmail.com>
2025-08-07 15:27:33 +0600

    simple pop login menu added

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,25 @@
+import { signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider } from "firebase/auth";
+import auth from "../../Firebase/Firebase.init";
+
 const Login = () => {
+
+    const provider = new GoogleAuthProvider();
+
+    const handleGoogleSignIn = () => {
+        // console.log("google coming soon");
+        signInWithPopup(auth, provider)
+        .then((result)  => {
+            console.log(result);
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    }
+
     return (
         <div>
-            <button>Login with google</button>
+            <button onClick={handleGoogleSignIn}>Login with google</button>
         </div>
     );
 };
